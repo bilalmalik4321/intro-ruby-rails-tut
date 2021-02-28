bec9fd9800ca8725b00182d88971f3f5b68e64d8
bilal-wsib <bilal@wsib-innovation.ca>
2021-02-28 13:39:30 -0500

    added routing though navbar, utilizinging partials

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,8 @@
+# right pertaining code we can use with index.html.erb, will be embedded ruby in html, from here
 class HomeController < ApplicationController
   def index
+  end
+
+  def about
   end
 end
