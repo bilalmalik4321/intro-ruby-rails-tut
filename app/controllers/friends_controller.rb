d8853a22aced933cb5ac8e1d25fc069452d46150
bilal-wsib <bilal@wsib-innovation.ca>
2021-04-11 17:54:37 -0400

    associations and additional style modifications

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,5 +1,9 @@
 class FriendsController < ApplicationController
   before_action :set_friend, only: %i[ show edit update destroy ]
+  # if user is not authenticated, dont let them use these routes, except for index and show friends (i think we use except due to the not !)
+  before_action :authenticate_user!, except: %i[ index show ]
+  # before running any of these functions, see if it is the correct users, only apply for pages where we are editing, updating or deleting
+  before_action :correct_user, only: %i[edit update destroy]
 
   # GET /friends or /friends.json
   def index
@@ -12,7 +16,9 @@ class FriendsController < ApplicationController
 
   # GET /friends/new
   def new
-    @friend = Friend.new
+    # not simply new friend, but associated with current user
+    # @friend = Friend.new
+    @friend = current_user.friends.build
   end
 
   # GET /friends/1/edit
@@ -21,7 +27,8 @@ class FriendsController < ApplicationController
 
   # POST /friends or /friends.json
   def create
-    @friend = Friend.new(friend_params)
+    # @friend = Friend.new(friend_params)
+    @friend = current_user.friends.build(friend_params)
 
     respond_to do |format|
       if @friend.save
@@ -56,6 +63,11 @@ class FriendsController < ApplicationController
     end
   end
 
+  def correct_user
+    @friend = current_user.friends.find_by(id: params[:id])
+    redirect_to friends_path, notice: "Not Authorized To Edit This Friend" if @friend.nil? 
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_friend
@@ -64,6 +76,6 @@ class FriendsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def friend_params
-      params.require(:friend).permit(:first_name, :last_name, :email, :phone, :twitter)
+      params.require(:friend).permit(:first_name, :last_name, :email, :phone, :twitter, :user_id)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   # resources route auto created, to automate for crud 
   resources :friends
   get 'home/about'
-  root 'home#index'
+  # original home#index took to homepage hello world
+  # root 'home#index'
+  root 'friends#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
